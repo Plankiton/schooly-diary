b282e8ccc81c46201083e0b5989aaa2786b8bccd
Yaks Souza <pl4nk1ton@gmail.com>
2022-06-12 16:47:59 -0300

    add support to classes into register

--- app.py
+++ app.py
@@ -31,36 +31,64 @@ def index():
 
 @app.route('/register/<sign_type>', methods=['GET', 'POST'])
 def register(sign_type):
-    if sign_type not in ['student', 'teacher']:
+    if sign_type not in ['student', 'teacher', "class"]:
         return redirect('/not_found', code=404)
 
     if request.method == 'POST':
+        if sign_type == "class":
+            st_class = request.form.to_dict()
+
+            students = []
+            remove = []
+            for key in st_class:
+                if "id" in key:
+                    print(key)
+                    students.append(key[key.index(".")+1:])
+                    remove.append(key)
+            for key in remove:
+                del st_class[key]
+
+            st_class["students"] = students
+            inserted = classes.insert_one(st_class)
+            return redirect(url_for('get_' + sign_type, id=inserted.inserted_id))
+
         person = request.form.to_dict()
         person['type'] = sign_type
-        print(person)
 
         remove_list = []
         notification_emails = []
+        if 'notification_email_count' in person:
+            del person['notification_email_count']
         for key in person:
             if 'notification_email' in key:
                 notification_emails.append(person[key])
                 remove_list.append(key)
         for key in remove_list:
             del person[key]
-        if 'notification_email_count' in person:
-            del person['notification_email_count']
         person['notification_emails'] = notification_emails
 
-        print(person)
         try:
             inserted = persons.insert_one(person)
-            if sign_type == 'student':
-                redirect(url_for('/student', id=inserted.inserted_id))
-            return redirect(url_for('login'))
+            if sign_type == 'teacher':
+                return redirect(url_for('login'))
+            return redirect(url_for('get_' + sign_type, id=inserted.inserted_id))
         except:
             return render_template('index.html', message='User Already Exists')
     else:
-        return render_template('register.html', sign_type=sign_type)
+        students_res = persons.find({ "type": "student" })
+        students = []
+        if sign_type == "class":
+            if students_res:
+                for st in students_res:
+                    print(st)
+                    students.append(dict(st))
+                i = 0
+                for student in students:
+                    student["old"] = 19
+                    student["class"] = "2 - A"
+                    students[i] = student
+                    i += 1
+        return render_template('register.html', sign_type=sign_type, students=students)
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
@@ -69,6 +97,7 @@ def login():
     else:
         e = request.form['email']
         p = request.form['password']
+        print(e, p)
         data = persons.find_one({"email": e, "password": p, "type": "teacher"})
         print(data)
         if data is not None:
@@ -81,6 +110,21 @@ def login():
 def logout():
     session['logged_in'] = False
     return redirect(url_for('index'))
+
+# @app.route('/<sign_type>/<id>')
+# def student(sign_type, id):
+#     print(sign_type, id)
+#     return f'{sign_type}/{id}'
+@app.route('/student/<id>')
+def get_student(id):
+    return f'student/{id}'
+@app.route('/class/<id>')
+def get_class(id):
+    return f'/class/{id}'
+@app.route('/teacher/<id>')
+def get_teacher(id):
+    return f'/teacher/{id}'
+
 
 if __name__ == '__main__':
     app.run()
--- main.py
+++
@@ -1,8 +0,0 @@
-from app import app, db
-from db import User
-
-if(__name__ == '__main__'):
-    app.secret_key = "ThisIsNotASecret:p"
-
-    db.create_all()
-    app.run()
